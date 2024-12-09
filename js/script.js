4f85173cb07378db230080ee6b4a9dac45f70260
MamanSuganda <70936274+MamanSuganda@users.noreply.github.com>
2024-12-09 14:11:45 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ let valueCahaya = document.querySelector(".value-cahaya");
     // Referensi ke data di Firebase
     const dataRef = database.ref('massages/suhu'); // Ganti 'dataPath' dengan path data di Firebase
     const dataRefK = database.ref('massages/Kelembapan')
-    const dataRefC = database.ref('massages/Cahaya')
+    const dataRefC = database.ref('massages/Cahaya/value')
 
     // Mendengarkan perubahan data di Firebase
     dataRef.on('value', (snapshot) => {
